9c177f0593bc9738864d1580d603b716a8622181
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-08-23 02:21:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,18 @@
 let handler = async m => m.reply(`
-┌〔 Donasi • Pulsa 〕
-├ Axis [083128734012]
-├ BYU [085157336614]
-└────
-
-┌〔 Donasi • Emoney 〕
-├ OVO, Dana [083128734012]
-├ https://saweria.co/ariffb
-└────
-`.trim())
+╭─「 Donasi • linkaja 」
+│ • hubungi owner ketik .owner]
+ |  • Saweria https://saweria.co/Doraemond
+ |   • Three [wa.me/089601145546]
+│ • Telkomsel [wa.me/6282387704390]
+ |  • Saya harap kamu menyukai bot saya
+ |      dan jangan lupa untuk mensuport saya
+ |     karena saya sangat menyukai dora
+ |     # by owner Dora :D
+ |       saya harap kamu bahagia
+╰────
+╭─「 Dorameond 
+╰────
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
