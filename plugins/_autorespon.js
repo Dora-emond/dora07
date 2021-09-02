791bd55e2530b75ef1210a41ec68ab1a768a7dd2
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-09-02 19:59:46 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -63,7 +63,7 @@ https://github.com/Dora-emond/dora07
     if (new Date() * 1 - setting.status > 1000) {
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
-        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | stikerinbot oleh ariffb`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} | dorabot oleh ®doraemond`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
