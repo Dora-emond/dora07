8eb2b89b3cd26d605b636fe78ba7d596ae86266c
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-10-11 21:09:02 +0700

    Create coinflip.js

---
+++ plugins/coinflip.js
@@ -0,0 +1,30 @@
+	let res = await axios.get(
+		API("https://no-api-key.com", "/api/v2/coin-flip")
+	);
+	if (res.status !== 200) throw await `${res.status} ${res.statusText}`;
+	const sticker = await createSticker(res.data.gif, {
+		type: StickerTypes.FULL,
+		pack: global.packname,
+		author: global.author,
+	});
+	await conn.sendMessage(m.chat, sticker, MessageType.sticker, {
+		quoted: m,
+		mimetype: "image/webp",
+	});
+	const head = await createSticker(res.data.image, {
+		type: StickerTypes.FULL,
+		pack: global.packname,
+		author: global.author,
+	});
+	await conn.sendMessage(m.chat, head, MessageType.sticker, {
+		quoted: m,
+		mimetype: "image/webp",
+	});
+	await m.reply(res.data.coin);
+};
+handler.help = ["coin", "koin"];
+handler.tags = ["fun"];
+
+handler.command = /^((coin|koin)?flip)$/i;
+
+module.exports = handler;
